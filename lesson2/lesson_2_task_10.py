14bae3dca40a614505d8d98e591e92eaa8871413
Steikus <stake72@yandex.ru>
2024-04-12 21:59:26 +0300

    few fixes hw2

--- lesson2/lesson_2_task_10.py
+++ lesson2/lesson_2_task_10.py
@@ -3,7 +3,7 @@ def bank():
     y = int(input("Количество лет: "))
     n = 1.1
     sum = x * n ** y
-    print("Накопленная сумма: ", round(sum))
+    print("Накопленная сумма: ", round(sum, 2))
 
 
 bank()
